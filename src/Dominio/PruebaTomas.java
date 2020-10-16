ee71158984bcb244dfc4a697a0963ca715edae79
BlaZzes10 <tomasjesus.garcia@alu.uclm.es>
2020-10-16 23:07:24 +0200

    Rama Development

--- src/Dominio/PruebaTomas.java
+++ src/Dominio/PruebaTomas.java
@@ -4,7 +4,7 @@ public class PruebaTomas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Prueba Tomás");
+		System.out.println("Rama de trabajo en la que se realiza la integración antes de pasar a Master");
 	}
 
 }
