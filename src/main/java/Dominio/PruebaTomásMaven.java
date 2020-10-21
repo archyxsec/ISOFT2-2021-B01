952f7a67c94e866f6f77d0c1a5a25ce35754da88
BlaZzes10 <tomasjesus.garcia@alu.uclm.es>
2020-10-21 23:52:20 +0200

    Solo queda configurar dependencias

--- src/main/java/Dominio/PruebaTomásMaven.java
+++ src/main/java/Dominio/PruebaTomásMaven.java
@@ -4,7 +4,7 @@ public class PruebaTomásMaven {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Maven");
+		System.out.println("Tomás prueba Maven");
 	}
 
 }
