05cf8a3f4c181253d44863c45957de4876a12d03
developer <developer@debian>
2020-10-21 23:28:30 +0200

    Formateando para maven

--- src/main/java/Dominio/PruebaTomas.java
+++ src/main/java/Dominio/PruebaTomas.java
@@ -1,10 +1,10 @@
-package Dominio;
+package main.java.Dominio;
 
 public class PruebaTomas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Prueba Tomás");
+		System.out.println("Prueba Tomï¿½s");
 	}
 
 }
