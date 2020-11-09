7ca854b7b71c6bdaf7d546c79572c52c471f45da
BlaZzes10 <tomi.carrion@gmail.com>
2020-11-09 22:19:37 +0100

    Modulo1 para iteración 1 creado

---
+++ Principal/Iteracion1/src/main/java/org/Iteracion1/App.java
@@ -0,0 +1,13 @@
+package org.Iteracion1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
