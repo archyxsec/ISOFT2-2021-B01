9cacc62ef16db72192cb24bcff05e1f8f5d8eda1
rubensolid <rubenpc93@gmail.com>
2020-12-01 11:30:31 +0100

    Prueba

--- Iteracion1/src/main/java/es/uclm/esi/isoft2b01/iteracion1/Presentacion/IU_AsignarMesa.java
+++ Iteracion1/src/main/java/es/uclm/esi/isoft2b01/iteracion1/Presentacion/IU_AsignarMesa.java
@@ -3,5 +3,6 @@ package es.uclm.esi.isoft2b01.iteracion1.Presentacion;
 public class IU_AsignarMesa {
 	public static void main(String [] args) {
 		System.out.println("Prueba iteracion 1");
+		System.out.println("Prueba desde Mac");	
 	}
 }
