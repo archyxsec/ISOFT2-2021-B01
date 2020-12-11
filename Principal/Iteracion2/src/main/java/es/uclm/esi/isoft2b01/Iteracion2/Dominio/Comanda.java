566fbb6dbbeb1f6786c86a34ad1f8d022bf70312
SergioSanchezIglesias <Sergio.Sanchez32@alu.uclm.es>
2020-12-11 18:42:41 +0100

    Código de la Iteración 3

--- Principal/Iteracion2/src/main/java/es/uclm/esi/isoft2b01/Iteracion2/Dominio/Comanda.java
+++ Principal/Iteracion2/src/main/java/es/uclm/esi/isoft2b01/Iteracion2/Dominio/Comanda.java
@@ -1,6 +1,22 @@
 package es.uclm.esi.isoft2b01.Iteracion2.Dominio;
 
 public class Comanda {
+	public String getPlato() {
+		return plato;
+	}
+
+	public void setPlato(String plato) {
+		this.plato = plato;
+	}
+
+	public String getBebida() {
+		return bebida;
+	}
+
+	public void setBebida(String bebida) {
+		this.bebida = bebida;
+	}
+
 	public String plato;
 	public String bebida;
 	
