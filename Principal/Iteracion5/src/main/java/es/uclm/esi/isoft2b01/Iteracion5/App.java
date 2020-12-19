4a9877126975c097ff91022e133415c25f8871e5
BlaZzes10 <tomasjesus.garcia@alu.uclm.es>
2020-12-19 23:36:02 +0100

    Iteración 5 creada

---
+++ Principal/Iteracion5/src/main/java/es/uclm/esi/isoft2b01/Iteracion5/App.java
@@ -0,0 +1,13 @@
+package es.uclm.esi.isoft2b01.Iteracion5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
